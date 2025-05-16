62749ce4b54a96781116696c521b601d9769873e
DmitriyGron <unvmj5105775@gmail.com>
2025-05-16 17:49:11 +0300

    test first commit

---
+++ HW-001/HW-001/Calc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace HW_001
+{
+    internal class Calc
+    {
+        private double _firstValue;
+        private double _secondValue;
+
+        public double result { get; private set; }
+
+        public Calc(double a, double b)
+        {
+            _firstValue = a;
+            _secondValue = b;
+        }
+
+        public double Add()
+        {
+            return result = _firstValue + _secondValue;
+        }
+        public double Subtraction()
+        {
+            return result = _firstValue - _secondValue;
+        }
+
+        public double Multiplication()
+        {
+            return result = _firstValue * _secondValue;
+        }
+
+        public double Division()
+        {
+            if (_secondValue == 0)
+            {
+                throw new ArgumentException("Ошибка деления на ноль");
+            }
+
+            return result = _firstValue / _secondValue;
+        }
+        public void ShowInfo(string operation)
+        {
+            Console.WriteLine($"{_firstValue} {operation} {_secondValue} = {result}");
+        }
+    }
+}
---
+++ HW-001/HW-001/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
+namespace HW_001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                Console.WriteLine("Enter first value: ");
+                double a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter second value: ");
+                double b = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Choose operation: ");
+                string chooseOperation = Console.ReadLine();
+
+                Calc calc = new Calc(a, b);
+
+                if (chooseOperation == string.Empty)
+                {
+                    Console.WriteLine("You have not selected an operation or it is incorrect");
+                }
+                else
+                {
+
+                    switch (chooseOperation)
+                    {
+                        case "+":
+                            calc.Add();
+                            calc.ShowInfo("+");
+                            break;
+                        case "-":
+                            calc.Subtraction();
+                            calc.ShowInfo("-");
+                            break;
+                        case "*":
+                            calc.Multiplication();
+                            calc.ShowInfo("*");
+                            break;
+                        case "/":
+                            calc.Division();
+                            calc.ShowInfo("/");
+                            break;
+                        default:
+                            Console.WriteLine("You have not selected a valid operation.");
+                            break;
+                    }
+                }
+                Console.WriteLine("Enter 'exit' if u want to leave or press 'enter' for continue");
+                string exit = Console.ReadLine();
+                if (exit == "exit")
+                    break;
+            }
+        }
+    }
+}
